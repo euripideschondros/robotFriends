bb77c3038807406e3524992ff0ea5b76b27013af
euripideschondros <xoneula@gmail.com>
2020-10-16 18:50:38 +0300

    Add files via upload

---
+++ src/Robots.js
@@ -0,0 +1,62 @@
+export const robots = [
+    {
+        id: 1,
+        name: 'Leanne Graham',
+        username: 'Bret',
+        email: 'Sincere@april.biz'
+    },
+    {
+        id: 2,
+        name: 'Ervin Howell',
+        username: 'Antonette',
+        email: 'Sdddffe@april.biz'
+    },
+    {
+        id: 3,
+        name: 'Anna',
+        username: 'Mitsos',
+        email: 'Sincere@april.biz'
+    },
+    {
+        id: 4,
+        name: 'Akis Graham',
+        username: 'Akis',
+        email: 'SiAkise@april.biz'
+    },
+    {
+        id: 5,
+        name: 'Evripidis',
+        username: 'George',
+        email: 'SinGeoe@april.biz'
+    },
+    {
+        id: 6,
+        name: 'Hlias',
+        username: 'Bret',
+        email: 'Sincere@april.biz'
+    },
+    {
+        id: 7,
+        name: 'Sakis',
+        username: 'Antonette',
+        email: 'Sdddffe@april.biz'
+    },
+    {
+        id: 8,
+        name: 'Takis',
+        username: 'Mitsos',
+        email: 'Sincere@april.biz'
+    },
+    {
+        id: 9,
+        name: 'Makis',
+        username: 'Akis',
+        email: 'SiAkise@april.biz'
+    },
+    {
+        id: 10,
+        name: 'Dimitris',
+        username: 'George',
+        email: 'SinGeoe@april.biz'
+    }
+];
